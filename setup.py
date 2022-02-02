f031edc936e8a888b8346e12c3fed7a1a6482778
Tomasz Szoszorek <tomasz.szoszorek@roche.com>
2022-02-02 19:39:45 +0100

    pypi

--- setup.py
+++ setup.py
@@ -1,21 +1,25 @@
 # -*- coding: utf-8 -*-
 
 from setuptools import setup
+import os.path
 
-from yamllint import (
-    __author__,
-    __license__,
-    APP_NAME,
-    APP_VERSION,
-    APP_DESCRIPTION,
-)
-
+readme = ""
+here = os.path.abspath(os.path.dirname(__file__))
+readme_path = os.path.join(here, "README.md")
+if os.path.exists(readme_path):
+    with open(readme_path, "rb") as stream:
+        readme = stream.read().decode("utf8")
 
 setup(
-    name=APP_NAME,
-    version=APP_VERSION,
-    author=__author__,
-    description=APP_DESCRIPTION.split("\n")[0],
-    long_description=APP_DESCRIPTION,
-    license=__license__,
+    name="presidio_cli",
+    version="0.0.1",
+    description="CLI tool that analyzes text with Presidio framework",
+    author="test",
+    author_email="test@test",
+    url="https://github.com/insightsengineering/presidio-cli",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: Apache License v2",
+        "Operating System :: OS Independent",
+    ],
 )
