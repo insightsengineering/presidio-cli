56db3d1838768fb31b98c9a1c62a814efae494e6
Tomasz Szoszorek <tomasz.szoszorek@roche.com>
2022-02-03 11:01:46 +0100

    pyPi

--- presidio_cli/__init__.py
+++ presidio_cli/__init__.py
@@ -2,10 +2,11 @@
 
 
 APP_NAME = "presidio_cli"
-APP_VERSION = "0.0.1"
+APP_VERSION = "0.0.2"
 APP_DESCRIPTION = __doc__
 
-__author__ = u"Roche"
-__copyright__ = u"Copyright 2021, Roche"
+__author__ = u"Dawid Rycerz, Tomasz Szoszorek"
+__author_email__=u"knightdave@protonmail.com, 42359321+tomszosz@users.noreply.github.com"
+__copyright__ = u"Copyright 2022, F. Hoffman-La Roche Ltd"
 __license__ = "Apache 2.0"
 __version__ = APP_VERSION
--- setup.py
+++ setup.py
@@ -2,6 +2,14 @@
 
 from setuptools import setup
 import os.path
+from presidio_cli import (
+    __author__,
+    __author_email__,
+    __license__,
+    APP_NAME,
+    APP_VERSION,
+    APP_DESCRIPTION,
+)
 
 readme = ""
 here = os.path.abspath(os.path.dirname(__file__))
@@ -11,13 +19,17 @@ if os.path.exists(readme_path):
         readme = stream.read().decode("utf8")
 
 setup(
-    name="presidio_cli",
-    version="0.0.1",
-    description="CLI tool that analyzes text with Presidio framework",
-    author="Dawid Rycerz, Tomasz Szoszorek",
-    author_email="knightdave@protonmail.com, 42359321+tomszosz@users.noreply.github.com",
+    name=APP_NAME,
+    version=APP_VERSION,
+    author=__author__,
+    author_email=__author_email__,
+    #description=APP_DESCRIPTION.split("\n")[0],
+    #"CLI tool that analyzes text with Presidio framework",
     url="https://github.com/insightsengineering/presidio-cli",
-    long_description=readme,
+    license=__license__,
+    description=APP_DESCRIPTION.split("\n")[0],
+    long_description=APP_DESCRIPTION,
+    #long_description=readme,
     long_description_content_type="text/markdown",
     classifiers=[
         "Programming Language :: Python :: 3",
