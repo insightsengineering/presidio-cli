fcdf1db975361d31ae70d176e16ec91f4b7e6f88
Dave <13086223+knightdave@users.noreply.github.com>
2022-02-04 17:01:02 +0100

    initial setup (#1)

---
+++ presidio_cli/__init__.py
@@ -0,0 +1,12 @@
+"""An python CLI for analyze PII Entities with Microsoft Presidio framework."""
+
+
+APP_NAME = "presidio_cli"
+APP_VERSION = "0.0.3"
+APP_DESCRIPTION = __doc__
+
+__author__ = u"Dawid Rycerz, Tomasz Szoszorek"
+__author_email__=u"basel.nestcicd@roche.com"
+__copyright__ = u"Copyright 2022, F. Hoffman-La Roche Ltd"
+__license__ = "Apache 2.0"
+__version__ = APP_VERSION
